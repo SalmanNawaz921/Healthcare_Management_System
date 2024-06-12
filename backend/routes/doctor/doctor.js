f83ccdbb87587422d693f8dbfc314a4dfb245489
Muhammad Salman <118910895+SalmanNawaz921@users.noreply.github.com>
2024-06-12 18:57:58 +0500

    removed unnecessary comments

--- backend/routes/doctor/doctor.js
+++ backend/routes/doctor/doctor.js
@@ -1,119 +1,41 @@
-const Prescription = require("../models/prescriptionModel");
-const doctor = require("../models/doctorModel");
-const patient = require("../models/patientModel");
-const Patient = require("../models/patientModel");
+const requireAuth = require("../../middlewares/requireAuth");
+const {
+  view_patients,
+  view_treatments_doctor,
+  view_doctor_vists,
+  view_appointments_doctor,
+} = require("../../controllers/fetchList");
 
-const editDetails = async (req, res) => {
-  let success = false;
-  try {
-    const { DoctorID } = req.user;
-    const params = req.body;
-    const doctors = await doctor.updateDoctor(DoctorID, params);
-    success = true;
-    res.status(200).json({ success, doctors });
-  } catch (error) {
-    res.status(400).json({ success, msg: error.message });
-  }
-};
-const getDetails = async (req, res) => {
-  try {
-    const { DoctorID } = req.user;
-    let doctors;
-    if (DoctorID) doctors = await doctor.findDoctor(DoctorID);
-    else doctors = await doctor.findDoctor(req.params.id);
-    if (doctors) res.status(200).json(doctors);
-  } catch (error) {
-    res.status(400).json({ msg: error.message });
-  }
-};
-const view_prescriptions_doctors = async (req, res) => {
-  try {
-    const { DoctorID } = req.user;
-    const prescriptions = await Prescription.getAllPrescriptionsDoctor(
-      DoctorID
-    );
-    res.status(200).json(prescriptions);
-  } catch (error) {
-    res.status(400).json({ msg: error.message });
-  }
-};
-
-const add_prescriptions = async (req, res) => {
-  try {
-    const { DoctorID } = req.user;
-    const params = req.body;
-    const prescriptions = await Prescription.addPrescription(params, DoctorID);
-    if (prescriptions) {
-      res.status(200).json(Patient.getAllPatients(DoctorID));
-    }
-  } catch (error) {
-    res.status(400).json({ msg: error.message });
-  }
-};
-
-const delete_prescriptions = async (req, res) => {
-  try {
-    const { DoctorID } = req.user;
-    const { PrescriptionID } = req.params;
-    const prescriptions = await Prescription.deletePrescription(
-      PrescriptionID,
-      DoctorID
-    );
-    res.status(200).json(prescriptions);
-  } catch (error) {
-    res.status(400).json({ msg: error.message });
-  }
-};
-
-const edit_prescriptions = async (req, res) => {
-  try {
-    const { DoctorID } = req.user;
-    const { PrescriptionID } = req.params;
-    const params = req.body;
-    const prescriptions = await Prescription.editPrescription(
-      params,
-      PrescriptionID,
-      DoctorID
-    );
-    res.status(200).json(prescriptions);
-  } catch (error) {
-    res.status(400).json({ msg: error.message });
-  }
-};
-
-const get_doctor_earnings = async (req, res) => {
-  try {
-    const { DoctorID } = req.user;
-    const earnings = await doctor.getEarnings(DoctorID);
-    res.status(200).json(earnings);
-  } catch (error) {
-    res.status(400).json({ msg: error.message });
-  }
-};
-
-const get_patient_symptoms = async (req, res) => {
-  try {
-    const {id}=req.params;
-    let symptoms;
-    if(id){
-       symptoms = await patient.getPatientSymptoms(id);
-      }
-      else{
-        const { PatientID } = req.user;
-        symptoms = await patient.getPatientSymptoms(PatientID);
-    }
-    res.status(200).json(symptoms);
-  } catch (error) {
-    res.status(400).json({ msg: error.message });
-  }
-};
-
-module.exports = {
+const express = require("express");
+const {
   editDetails,
   getDetails,
   view_prescriptions_doctors,
   add_prescriptions,
   edit_prescriptions,
   delete_prescriptions,
-  get_patient_symptoms,
-};
+} = require("../../controllers/doctor");
+const route = express.Router();
+route.use(express.json());
+
+route.get("/patientlist", requireAuth("doctor"), view_patients);
+route.get("/treatmentlist", requireAuth("doctor"), view_treatments_doctor);
+route.put("/editDetails", requireAuth("doctor"), editDetails);
+route.get("/getDetails/:id", requireAuth(["doctor","hospitalAdmin","patient"]), getDetails);
+route.get("/viewVisits", requireAuth("doctor"), view_doctor_vists);
+
+// Prescription Routes
+
+route.get("/prescriptions", requireAuth("doctor"), view_prescriptions_doctors);
+route.post("/addPrescription", requireAuth("doctor"), add_prescriptions);
+route.put("/editPrescription", requireAuth("doctor"), edit_prescriptions);
+route.delete("/deltePrescription", requireAuth("doctor"), delete_prescriptions);
+
+// Appointment Routes
+route.get(
+  "/getAppointmentsDoctor",
+  requireAuth("doctor"),
+  view_appointments_doctor
+);
+
+module.exports = route;
