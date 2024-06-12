c6462398ce8dac37b69c42f3ac69d7f723878d55
Muhammad Salman <118910895+SalmanNawaz921@users.noreply.github.com>
2024-06-12 18:57:06 +0500

    updated doctor.js

--- backend/controllers/doctor.js
+++ backend/controllers/doctor.js
@@ -93,8 +93,15 @@ const get_doctor_earnings = async (req, res) => {
 
 const get_patient_symptoms = async (req, res) => {
   try {
-    const { PatientID } = req.user;
-    const symptoms = await patient.getPatientSymptoms(PatientID);
+    const {id}=req.params;
+    let symptoms;
+    if(id){
+       symptoms = await patient.getPatientSymptoms(id);
+      }
+      else{
+        const { PatientID } = req.user;
+        symptoms = await patient.getPatientSymptoms(PatientID);
+    }
     res.status(200).json(symptoms);
   } catch (error) {
     res.status(400).json({ msg: error.message });
